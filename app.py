8cdd3715b1a3c65cb4d8f082a2f49473b4b37410
Edwiin22 <20243rd034@utez.edu.mx>
2025-11-05 16:20:11 -0600

    Add files via upload
    hice modificaciones porque no funcionaba

--- app.py
+++ app.py
@@ -1,52 +1,83 @@
-from flask import Flask, jsonify, render_template_string, request, redirect, url_for
+from flask import Flask, request
 
 app = Flask(__name__)
 
 dispositivos = {}
 
-@app.route('/dispositivo', methods=['GET'])
-def mostrar_dispositivo():
+@app.route('/dispositivos_mostrar', methods=['GET'])
+def mostrar_dispositivos_html():
     html = """
     <html>
-     <head>
-    <tittle> Dispositivos </tittle>
-    <style> .dispositivo
-    border: 2px;
-    header: 10px;
-    backgroun-color: #222222
-    </style>
-     </head>
-
-     <body>
-     <h1> Lista de Dispositivos </h1>
-     <p>ID: </P>
-     </br>
-     <p>Nombre: </p>
-    <p>descripcion:</P>
-    <p> IP: </P>
-    <p> MAC: </p>
-    <p> Ubicacion: </p>
-    <p> Tipo: </p>
-    <p> Otros: </p>
+    <head>
+        <title>Dispositivos de red</title>
+    </head>
+    <body>
+        <h2>Lista de dispositivos</h2>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Descripción</th>
+                    <th>IP</th>
+                    <th>MAC</th>
+                    <th>Ubicación</th>
+                    <th>Tipo</th>
+                    <th>Otros</th>
+                </tr>
+            </thead>
+            <tbody>
+    """
+    for dispositivo_id, dispositivo in dispositivos.items():
+        html += f"""
+            <tr>
+                <td>{dispositivo_id}</td>
+                <td>{dispositivo['nombre']}</td>
+                <td>{dispositivo['descripcion']}</td>
+                <td>{dispositivo['ip']}</td>
+                <td>{dispositivo['mac']}</td>
+                <td>{dispositivo['ubicacion']}</td>
+                <td>{dispositivo['tipo']}</td>
+                <td>{dispositivo['otros']}</td>
+            </tr>
+        """
+    html += """
+            </tbody>
+        </table>
+    </body>
     </html>
     """
-
-for dispositivoID in dispositivos():
-    html += f"""
-<p>{dispositivos.get(id)}</p>
-<p>{dispositivos.get(Nombre)}</P>
-<P>{dispositivos.get(descripcion)}</p>
-<p>{dispositivos.get(IP)}</P>
-<p>{dispositivos.get(Mac)}</p>
-<p>{dispositivos.get(Ubicacion)}</p>
-<p>{dispositivos.get(Tipo)}</p>
-<p>{dispositivos.get(otros)}</p>
-"""
-   
+    return html
 
 @app.route('/dispositivos', methods=['POST'])
-def agregar_Dispositivo():
-    data = request.get_json()
-    dispositivoID = data.get('id')
-    if dispositivoID in dispositivos:
-        return jsonify
+def agregar_dispositivo():
+    nuevo = request.get_json()
+    dispositivos[nuevo['id']] = {
+        'nombre': nuevo['nombre'],
+        'descripcion': nuevo['descripcion'],
+        'ip': nuevo['ip'],
+        'mac': nuevo['mac'],
+        'ubicacion': nuevo['ubicacion'],
+        'tipo': nuevo['tipo'],
+        'otros': nuevo.get('otros', '')
+    }
+    return {"mensaje": "Dispositivo agregado correctamente"}, 201
+
+@app.route('/dispositivos/<id>', methods=['PUT'])
+def modificar_dispositivo(id):
+    if id not in dispositivos:
+        return {"error": "No encontrado"}, 404
+    datos = request.get_json()
+    dispositivos[id].update({
+        'nombre': datos.get('nombre', dispositivos[id]['nombre']),
+        'descripcion': datos.get('descripcion', dispositivos[id]['descripcion']),
+        'ip': datos.get('ip', dispositivos[id]['ip']),
+        'mac': datos.get('mac', dispositivos[id]['mac']),
+        'ubicacion': datos.get('ubicacion', dispositivos[id]['ubicacion']),
+        'tipo': datos.get('tipo', dispositivos[id]['tipo']),
+        'otros': datos.get('otros', dispositivos[id]['otros'])
+    })
+    return {"mensaje": "Dispositivo actualizado correctamente"}
+
+if __name__ == '__main__':
+    app.run(debug=True)
